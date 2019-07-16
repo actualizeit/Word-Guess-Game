9ca3c5ee7002b02e2c42bcdd412f8e6c71421beb
dbell <greenadvisor@gmail.com>
2019-07-16 15:35:51 -0400

    win condition triggering on submit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // variables for potential letters and the words that may be selected to guess
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var words = ["nuts", "darn", "gosh"];
+var words = ["nuts", "darn", "gosh", "what", "omfg"];
 
 // Creating a variable to hold the number of wins. Starts at 0.
 var wins = 0;
@@ -12,11 +12,22 @@ var lettersGuessed = [];
 // Randomly chooses a word from the words array. This is the word to be guessed.
 var currentWord = words[Math.floor(Math.random() * words.length)];
 
+// An array variable to keep track of the words seen already
+
+var wordsSeen = [currentWord];
+
 // Creates an array from the word to be guessed and sets it as a variable
 var currentWordArray = Array.from(currentWord);
 
 // Creates an array of blanks the length of the word to be guessed
 var blanks = new Array(currentWordArray.length).fill("_");
+
+// Creates a function to count the occurances of an input in an array
+function getOccurrence(array, value) {
+  var count = 0;
+  array.forEach((v) => (v === value && count++));
+  return count;
+}
 
 
 console.log(currentWordArray);
@@ -26,7 +37,9 @@ console.log(currentWordArray);
 var directionsText = document.getElementById("directions-text");
 var winsText = document.getElementById("wins-text");
 var currentWordText = document.getElementById("current-word-text");
-var letterGuessedText = document.getElementById("letters-guessed-text");
+var guessesRemainingText = document.getElementById("guesses-remaining-text");
+var lettersGuessedText = document.getElementById("letters-guessed-text");
+var youWonText = document.getElementById("you-won-text");
 
 
 // This function is run whenever the user presses a key.
@@ -38,35 +51,60 @@ document.onkeyup = function(event) {
   // Only run the following code block if the user presses a key in the letters array.
   if (letters.includes(userGuess)){
 
+    // Check that the word has not been guessed before proceeding.
+    if(getOccurrence(blanks, "_") == 1 && currentWordArray.indexOf(userGuess) == blanks.indexOf("_"))
+    { 
+    // Incriment wins and resets the game if the word has been guessed
+      wins++;
+      w = words.indexOf(currentWord);
+      blanks = new Array(currentWordArray.length).fill("_");
+      guesses = 12;
+      lettersGuessed = [];
+      if(i < words.length - 1){
+        i++;
+      }else{
+      w = 0;
+      }
+    currentWord = words[w];
+
+    youWonText = "You Won! That's pretty much amazing. Why don't you try again? Guess another letter to continue!";
+
+    }else{
+  
+
+    // console.log("index _ in blanks = " + blanks.indexOf("_"));
+    console.log("Letters Guessed userGuess index: " + lettersGuessed.indexOf(userGuess));
+    console.log("blanks userGuess index: " + blanks.indexOf(userGuess));
+    console.log(currentWordArray);
+
     if(currentWordArray.includes(userGuess) && blanks.indexOf(userGuess) < 0) {
         i = currentWordArray.indexOf(userGuess);
         blanks[i] = userGuess;
-        console.log("index of blanks = " + blanks.indexOf("_"));
+    }
 
-  // Incriment wins and reset the game if all the blanks are filled
-      if(blanks.indexOf("_") < 0){
-        console.log(blanks);
-        wins++;
-        blanks = new Array(currentWordArray.length).fill("_");
-        currentWord = words[Math.floor(Math.random() * words.length)];
-        guesses = 12;
-        lettersGuessed = [];
-}
-    }else{
+
   // Take away a guess if the letter is not in the currentWordArray and add the letter to letters guessed
         if(lettersGuessed.indexOf(userGuess) < 0 && (blanks.indexOf(userGuess) < 0)){
         guesses--;
         lettersGuessed.push(userGuess);
-        console.log(guesses);
+        if(guesses <= 0){
+          blanks = new Array(currentWordArray.length).fill("_");
+          currentWord = words[Math.floor(Math.random() * words.length)];
+          guesses = 12;
+          lettersGuessed = [];
         }
-    }
 
-    // Hide the directions
+        }
+      }
+        // Hide the directions
     directionsText.textContent = "";
 
     // Display the .
     winsText.textContent = "Wins: " + wins;
-    currentWordText.textContent = blanks; //.join(" ")
-    letterGuessedText.textContent = lettersGuessed;
+    currentWordText.textContent = "Current Word: " + blanks.join(" ");
+    guessesRemainingText.textContent = "Guesses Remaining: " + guesses;
+    lettersGuessedText.textContent = "Letters Guessed: " + lettersGuessed.join(", ");
+
+
   };
 };
