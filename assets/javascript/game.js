a67286b40e53cd4c3580053eaf2c483651650947
dbell <greenadvisor@gmail.com>
2019-07-15 22:41:42 -0400

    initial references in place

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,32 @@
-var computerChoices = ["r", "p", "s"];
+// variables for potential letters and the words that may be selected to guess
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+var words = ["nuts", "darn", "gosh"];
 
-// Creating variables to hold the number of wins, losses, and ties. They start at 0.
+// Creating a variable to hold the number of wins. Starts at 0.
 var wins = 0;
-var losses = 0;
-var ties = 0;
 
-// Create variables that hold references to the places in the HTML where we want to display things.
+
+// Variables that hold references to the places in the HTML where we want to display things.
+
 var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("userchoice-text");
-var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var tiesText = document.getElementById("ties-text");
+var guessesText = document.getElementById("guesses-text");
+var LetterGuessedText = document.getElementById("letters-guessed-text");
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
 
-  // Determines which key was pressed.
-  var userGuess = event.key;
+    // Determines which key was pressed.
+    var userGuess = event.key;
 
-  // Randomly chooses a choice from the options array. This is the Computer's guess.
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  // Randomly chooses a word from the words array. This is the word to be guessed.
+    var currentWord = words[Math.floor(Math.random() * words.length)];
 
-  // Only run the following code block if the user presses "r" or "p" or "s".
-  if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+  // Creates an array from the word to be guessed and sets it as a variable
+    var currentArray = Array.from()
+
+  // Only run the following code block if the user presses a key in the letters array.
+  if (letters.indexOf(userGuess) >= 0){
 
     // If we choose rock and the computer guesses scissors, increment our wins variable.
     if ((userGuess === "r") && (computerGuess === "s")) {
@@ -59,7 +62,7 @@ document.onkeyup = function(event) {
     if (userGuess === computerGuess) {
       ties++;
     }
-
+    }
     // Hide the directions
     directionsText.textContent = "";
 
